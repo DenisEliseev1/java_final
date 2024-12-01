89fb787b069d1cf6b9d9f69e4f665772edf19ffd
DenisEliseev1 <khandagay@gmail.com>
2024-12-01 18:24:52 +0800

    version 1.1 work wave

--- program.java
+++ program.java
@@ -3,11 +3,11 @@ public class program {
         String name = "map.txt";
         //Character [][] arr = generateMap.arrayMap(name);
         int arr1 [][] = generateMap.arrayMapInt(name);
-        printarray.printTerminal(arr1, generateMap.lengthMap(name), generateMap.weidhtMap (name)); 
+        //printarray.printTerminal(arr1, generateMap.lengthMap(name), generateMap.weidhtMap (name)); 
         //System.out.println(arr1 [1][0]);
         //System.out.println(arr1 [0][2]);        
         int [][] arr2 = waveMethod.start(arr1, generateMap.lengthMap(name), generateMap.weidhtMap (name));    
-        printarray.printTerminal(arr2, generateMap.lengthMap(name), generateMap.weidhtMap (name)); 
+        //printarray.printTerminal(arr2, generateMap.lengthMap(name), generateMap.weidhtMap (name)); 
 
     }
 }
--- waveMethod.java
+++ waveMethod.java
@@ -27,37 +27,47 @@ public class waveMethod {
     }
     //   вар1 - точка В (проблема - нет всех путей)
     public static int [][] waveMapFstep (int [][] arrch,int x0, int y0, int x, int y, int xmax, int ymax) throws Exception {
-        if ((x > 0) && (arrch [x-1][y] != -1) && (x-1 != x0) && (arrch [x-1][y] == 0)) {
+        if ((x-1 >= 0) &&  (arrch [x-1][y] == 0)) {
+            if ((y != y0) || (x-1 != x0)) {
+                arrch [x-1][y] = arrch [x][y]+1;
+            }
             //System.out.println(1);
-            arrch [x-1][y] = arrch [x][y]+1;
-        } else if ((y+1 < ymax) && (arrch [x][y+1] != -1) && (y+1 != y0) && (arrch [x][y+1] == 0)) {
+        } if ((y+1 < ymax)  && (arrch [x][y+1] == 0)) {
+            if ((x != x0) || (y+1 != y0)) { 
             //System.out.println(2);
             arrch [x][y+1] = arrch [x][y]+1;;
             //System.out.println(arrch [x][y+1]);
-        } else if ((x+1 < xmax) && (arrch [x+1][y] != -1) && (x+1 != x0) && (arrch [x+1][y] == 0)) {
+            }
+        } if ((x+1 < xmax)  && (arrch [x+1][y] == 0)) {
+            if ((y != y0) ||  (x+1 != x0))
            //System.out.println(3);
             arrch [x+1][y]= arrch [x][y]+1;
-        } if ((y > 0) && (arrch [x][y-1] != -1) && (y-1 != y0) && (arrch [x][y-1] == 0)) {
+        } if ((y-1 >= 0) && (arrch [x][y-1] == 0)) {
+            if ((x != x0) || (y-1 != y0)) {
+                arrch [x][y-1]+= arrch [x][y]+1;
+            }
             //System.out.println(4);
-            arrch [x][y-1]+= arrch [x][y]+1;}
+ }
             //System.out.println(arrch [x][y-1]);
+        printarray.printTerminal(arrch, xmax, ymax);
+        System.out.println();
         return arrch;
     }
     public static int [][] waveMapStep (int [][] arrch,int x0, int y0, int xmax, int ymax, int r) throws Exception {
         int flag = 0;
-        for (int i = 0; i < xmax; i++) {
-            for (int j = 0; j < ymax; j++) {
-                if ((arrch [i][j] == 0) && (i != x0) && (j != y0)) {
+        for (int x = 0; x < xmax; x++) {
+            for (int y = 0; y < ymax; y++) {
+                if ((arrch [x][y] == 0) && (x != x0) && (y != y0)) {
                     flag++;
                 }
-                if (arrch [i][j] == r) {
-                    arrch = waveMapFstep(arrch, x0, y0, i, j, xmax, ymax);
+                if (arrch [x][y] == r) {
+                    arrch = waveMapFstep(arrch, x0, y0, x, y, xmax, ymax);
                 }
             }
         }
         r++;
         if (flag > 0) { 
-            waveMapStep (arrch, x0, y0, xmax, ymax,r);//????????????????????????????????????????????????
+            arrch = waveMapStep (arrch, x0, y0, xmax, ymax,r);//????????????????????????????????????????????????
         }
         return arrch;
     }
