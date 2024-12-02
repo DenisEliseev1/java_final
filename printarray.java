3f7878bc886763068e1217789f3b00e73fcd24d9
DenisEliseev1 <khandagay@gmail.com>
2024-12-02 15:23:34 +0800

    version 1.4 result file

--- printarray.java
+++ printarray.java
@@ -1,3 +1,6 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class printarray {
     public static void printTerminal(int arrch[][], int xmax, int ymax) {
         for (int i = 0; i < xmax; i++) {
@@ -16,5 +19,22 @@ public class printarray {
                 System.out.printf("%s%s", arrch[i][j], ' ');
             }
         }
+        
+        try (FileWriter res = new FileWriter("result.txt", false)) {
+            res.write("Result \n");
+            for (int i = 0; i < xmax; i++) {
+                for (int j = 0; j < ymax; j++) {
+                    res.append(arrch [i][j]);
+                }
+            res.append('\n');
+            }
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        /** 
+            * Здесь создать файл result<data>. txt
+            * сделать красивый вывод результата в txt. Желательно с начальным
+        */
     }
 }
